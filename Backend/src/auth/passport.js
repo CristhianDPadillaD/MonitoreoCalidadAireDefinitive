6e02cd2311934dd6c896f501784df9832510d8a9
CristhianDPadillaD <130010339+CristhianDPadillaD@users.noreply.github.com>
2026-02-15 17:24:24 -0500

    Permitir que solo usuarios de la Universidad entren

--- Backend/src/auth/passport.js
+++ Backend/src/auth/passport.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import fs from 'fs';
 import User from '../models/usersModel.js';
 export default function configurePassport() {
-  
+
 
   dotenv.config();
   if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET || !process.env.GOOGLE_CALLBACK_URL) {
@@ -26,7 +26,7 @@ export default function configurePassport() {
   }
 
   console.info('Configuring Passport Google strategy with callback:', callbackURL);
-  console.info('Google Client ID loaded:', clientID ? `${clientID.slice(0,8)}...` : 'missing');
+  console.info('Google Client ID loaded:', clientID ? `${clientID.slice(0, 8)}...` : 'missing');
 
   passport.use(
     new GoogleStrategy(
@@ -38,22 +38,28 @@ export default function configurePassport() {
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
-        const email = profile.emails[0].value;
+          const email = profile.emails[0].value;
+          const allowedDomain = "@umariana.edu.co";
+          if (!email.endsWith(allowedDomain)) {
+            console.log("Acceso denegado:", email);
+            return done(null, false, {
+              message: "Solo correos institucionales permitidos"
+            });
+          }
+          let user = await User.findOne({ email });
 
-        let user = await User.findOne({ email });
-
-        if (!user) {
-          user = await User.create({
-            googleId: profile.id,
-            name: profile.displayName,
-            email
-          });
+          if (!user) {
+            user = await User.create({
+              googleId: profile.id,
+              name: profile.displayName,
+              email
+            });
+          }
+          return done(null, user);
+        } catch (err) {
+          return done(err, null);
         }
-        return done(null, profile);
-      }catch (err) {
-        return done(err, null);
-        }
-        }
+      }
     )
   );
 
